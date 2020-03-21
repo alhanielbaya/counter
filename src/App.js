b9fa0b0db3cf7af9337646104fac91401ca6873d
alhanielbaya <alstrom201@gmail.com>
2020-03-20 17:47:39 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [boxes, setBoxes] = useState(0);
+  console.log(boxes);
+
+  function updateBox(num) {
+    localStorage.setItem('boxes', num);
+  }
+
+  function handleAdd() {
+    const num = boxes + 1;
+    setBoxes(num);
+    updateBox(num);
+  }
+  function handleMinus() {
+    const num = boxes - 1;
+    setBoxes(num);
+    updateBox(num);
+  }
+
+  useEffect(() => {
+    const localBoxes = localStorage.getItem('boxes');
+    if (localBoxes !== 'undefined') {
+      setBoxes(parseInt(localBoxes));
+    } else {
+      setBoxes(0);
+    }
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <button className='minus btn btn-lg btn-primary' onClick={handleMinus}>
+        <i className='icon icon-minus btn btn-primary'></i>
+      </button>
+      <div className='number'>{boxes}</div>
+      <button className='plus btn btn-lg btn-primary' onClick={handleAdd}>
+        <i className='icon icon-plus btn btn-primary'></i>
+      </button>
     </div>
   );
 }
